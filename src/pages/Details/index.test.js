2ea8e97c3d72fe1b028087f49e82e7171c086608
votantai <taivo.it2210@gmail.com>
2021-04-05 09:01:27 +0700

    add test

--- src/pages/Details/index.test.js
+++ src/pages/Details/index.test.js
@@ -2,7 +2,13 @@ import React from 'react';
 import Detail from './index';
 import renderer from 'react-test-renderer';
 
-jest.mock('../../components/VirtualTable', () => 'VirtualTable')
+jest
+  .mock('../../components/VirtualTable', () => 'VirtualTable')
+  .mock('umi', () => ({
+    connect: jest.fn(),
+  }))
+
+
 
 describe('<Detail />', () => {
   it('render <Detail />', () => {
